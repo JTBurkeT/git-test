d34a0e3bf7b06582030da431e47b3b7171817d0e
JTBurkeT <88762440+JTBurkeT@users.noreply.github.com>
2022-11-06 21:41:31 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,real hot-fix");
         System.out.println("hello,master");
         System.out.println("1111111111111111");
+        System.out.println("pull test");
     }
 }
